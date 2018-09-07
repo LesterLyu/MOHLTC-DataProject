2af52e72272b0cc753b520bb1ab94ab7e9cd9df4
Lester Lyu <lester.lyu@ontario.ca>
2018-09-07 09:36:17 -0400

    Fix Coding Styles, using Webstorm default style.

--- models/query.js
+++ models/query.js
@@ -18,26 +18,22 @@ var pool = mysql.createPool({
     database: 'dataproject'
 });
 module.exports = {
-  newQuery: function(query, callback)
-  {
-      pool.getConnection(function(err, connection) {
-      if (err) return callback(err);
-      pool.query(query, function(error, data)
-      {
-      if(error)
-      {
-        connection.release();
-        console.error(error);
-        callback(error, data);
-      }
-      else
-      {
-          console.log("Query success: " + query);
-          connection.release();
-          callback(error, data);
-      }
-      })
-      });
+    newQuery: function (query, callback) {
+        pool.getConnection(function (err, connection) {
+            if (err) return callback(err);
+            pool.query(query, function (error, data) {
+                if (error) {
+                    connection.release();
+                    console.error(error);
+                    callback(error, data);
+                }
+                else {
+                    console.log("Query success: " + query);
+                    connection.release();
+                    callback(error, data);
+                }
+            })
+        });
 
-  }
+    }
 }
--- models/sendmail.js
+++ models/sendmail.js
@@ -3,39 +3,39 @@ var nodeMailer = require('nodemailer');
 var express = require('express'); //idk how useful this is. DELETE if unnecessary
 
 var smtpTransport = nodeMailer.createTransport({
-  service: "gmail", //hostname
-  host: "smtp.gmail.com",
-  auth: {
-    user: "haodasdemo@gmail.com",
-    pass: "godisdeadgodremainsdeadandwehavekilledhim"
-  }
+    service: "gmail", //hostname
+    host: "smtp.gmail.com",
+    auth: {
+        user: "haodasdemo@gmail.com",
+        pass: "godisdeadgodremainsdeadandwehavekilledhim"
+    }
 })
 module.exports = {
-  sendFromHaodasMail: function(email, subject, message, callback) {
-    var mailConfig = {
-      to : email,
-      subject: subject,
-      text: message
-    };
+    sendFromHaodasMail: function (email, subject, message, callback) {
+        var mailConfig = {
+            to: email,
+            subject: subject,
+            text: message
+        };
 
-    smtpTransport.sendMail(mailConfig, function(err, response) {
-      if (err) {
-        console.log(err);
-      }
-      else {
-        console.log("Email sent: " + message);
-        callback();
-      }
-    })
-  },
+        smtpTransport.sendMail(mailConfig, function (err, response) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log("Email sent: " + message);
+                callback();
+            }
+        })
+    },
 
-  sendDemoMail: function(callback) {
-    var email = "haoda.fan@outlook.com";
-    var subject = "THIS CASTLE IS IN UNACCEPTABLE CONDITION!";
-    var message = "UNACCEPTABLE!!!!!!!!!!!!!!!!!!!";
+    sendDemoMail: function (callback) {
+        var email = "haoda.fan@outlook.com";
+        var subject = "THIS CASTLE IS IN UNACCEPTABLE CONDITION!";
+        var message = "UNACCEPTABLE!!!!!!!!!!!!!!!!!!!";
 
-    sendFromHaodasMail(email, subject, message, function() {
-      callback();
-    });
-  }
+        sendFromHaodasMail(email, subject, message, function () {
+            callback();
+        });
+    }
 }
