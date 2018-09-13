1ae057dda3da6a84bf6edb95412d6df382d662d9
Lester Lyu <lester.lyu@ontario.ca>
2018-09-13 14:01:38 -0400

    Add email config

--- config/config.js
+++ config/config.js
@@ -1,4 +1,12 @@
 module.exports = {
     database:"mongodb://127.0.0.1/dataproject",
     superSecret:"df;gjk3409tgpofdbvkw4gk-rwit24t,gdfpovoqeo[13- -3i -3 i-0ids-f-i-ak123-i 1--sH(*n( *y (*y(W#(*jhSD)*D)*SJ_(U#n_DXJ_ d",
+    mail: {
+        service: "gmail", //hostname
+        host: "smtp.gmail.com",
+        auth: {
+            user: "username@gmail.com",
+            pass: "password"
+        }
+    }
 };
