52234bbba0cf72762eb5abe746f43b356b50eadc
Lester Lyu <lester.lyu@ontario.ca>
2018-09-14 10:16:54 -0400

    Update schema

--- models/enteredForm.js
+++ models/enteredForm.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 let enteredFormSchema = new mongoose.Schema({
     title: String,
+    username: String,
     data: [],
+    date: {default: Date.now}
 });
 module.exports = mongoose.model('Attribute', enteredFormSchema);
