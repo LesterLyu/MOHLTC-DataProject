4fa03fe492a315ab46bfaa6d49e200a86a5a2bed
Lester Lyu <lester.lyu@ontario.ca>
2018-11-09 16:03:16 -0500

    Add shadow on tab scrolling switchers

--- public/dist/fillWorkbook.js
+++ public/dist/fillWorkbook.js
@@ -93,7 +93,7 @@ $(document).ready(function () {
 
       console.log('unzipping takes: ' + (new Date() - start) + 'ms');
       console.log(data);
-      gui = new WorkbookGUI('view', workbookName, data, extra, $(window).height() - 295);
+      gui = new WorkbookGUI('view', workbookName, data, extra, $(window).height() - 305);
       gui.load();
     }
   }).fail(function (xhr, status, error) {
