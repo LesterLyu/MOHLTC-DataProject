3b2524da41ef13e94ad96913d09486242cfbb70c
Lester Lyu <lester.lyu@ontario.ca>
2018-11-27 16:02:41 -0500

    Add Workbook query page, add more api to support.

--- controller/workbook-query.js
+++ controller/workbook-query.js
@@ -29,9 +29,9 @@ async function getAllUsersAtGroup(groupNumber) {
     return users; // [{username: '...'}, ...]
 }
 
-async function getDataAtLocation(onlyFilled, groupNumber, wbName, sheetName, catId, attId) {
-    let attMap, catMap, wbData, wsNames;
-    let result = [];
+async function getDataAtLocation(includeAttCatId, onlyFilled, groupNumber, wbName, queryData) {
+    let attMap, catMap, wbData, wsName2SheetNo;
+    let result = {};
     await Workbook.findOne({name: wbName, groupNumber: groupNumber},
         {name: 1, data: 1, attMap: 1, catMap: 1}).then((workbook) => {
         if (!workbook) {
@@ -41,17 +41,25 @@ async function getDataAtLocation(onlyFilled, groupNumber, wbName, sheetName, cat
             attMap = workbook.attMap;
             catMap = workbook.catMap;
             wbData = workbook.data;
-            wsNames = Object.entries(workbook.data).reduce((map, obj) => {
+            wsName2SheetNo = Object.entries(workbook.data).reduce((map, obj) => {
                 map[obj[1].name] = obj[0];
                 return map;
             }, {});
         }
     });
 
-    const sheetNo = wsNames[sheetName];
-    const row = catMap[sheetNo][catId], col = attMap[sheetNo][attId];
-    const dataKey = ['data', sheetNo, row, col].join('.');
-    const projector = {username: 1, [dataKey]: 1};
+    const projector = {username: 1, name: 1};
+    for (let wsName in queryData) {
+        const currSheet = queryData[wsName];
+        for (let i = 0; i < currSheet.length; i++) {
+            const sheetNo = wsName2SheetNo[wsName];
+            const catId = currSheet[i][0];
+            const attId = currSheet[i][1];
+            const row = catMap[sheetNo][catId], col = attMap[sheetNo][attId];
+            const dataKey = ['data', sheetNo, row, col].join('.');
+            projector[dataKey] = 1;
+        }
+    }
 
     await FilledWorkbook.find({name: wbName, groupNumber: groupNumber}, projector).then((filledWorkbooks) => {
 
@@ -62,57 +70,103 @@ async function getDataAtLocation(onlyFilled, groupNumber, wbName, sheetName, cat
                 // TO-DO
             }
         }
-        for (let i = 0; i < filledWorkbooks.length; i++) {
-            let data;
-            if (filledWorkbooks[i].data[sheetNo][row] && filledWorkbooks[i].data[sheetNo][row][col] !== undefined) {
-                data = filledWorkbooks[i].data[sheetNo][row][col]
-            }
-            else {
-                data = '';
-            }
-            result.push({
-                username: filledWorkbooks[i].username,
-                catId: catId,
-                attId: attId,
-                data: data,
-            });
 
+        for (let wsName in queryData) {
+            const currSheet = queryData[wsName];
+            result[wsName] = [];
+            for (let i = 0; i < currSheet.length; i++) {
+                const sheetNo = wsName2SheetNo[wsName];
+                const catId = currSheet[i][0], attId = currSheet[i][1];
+                const row = catMap[sheetNo][catId], col = attMap[sheetNo][attId];
+                const currCatAtt = [];
+                for (let i = 0; i < filledWorkbooks.length; i++) {
+                    let data;
+                    if (filledWorkbooks[i].data[sheetNo][row] && filledWorkbooks[i].data[sheetNo][row][col] !== undefined) {
+                        data = filledWorkbooks[i].data[sheetNo][row][col];
+                        if (typeof data === "object" && 'result' in data) {
+                            data = data.result;
+                        }
+                    }
+                    else {
+                        data = '';
+                    }
+                    if (includeAttCatId) {
+                        currCatAtt.push({
+                            username: filledWorkbooks[i].username,
+                            catId: catId,
+                            attId: attId,
+                            data: data,
+                        });
+                    }
+                    else {
+                        currCatAtt.push({
+                            username: filledWorkbooks[i].username,
+                            data: data,
+                        });
+                    }
+
+                }
+                result[wsName].push(currCatAtt);
+            }
         }
-
     });
-    return result; // Array of objects
+    return result;
 }
 
 module.exports = {
     checkPermission: checkPermission,
 
-    workbook_query: (req, res, next) => {
+    workbook_query: async (req, res, next) => {
         if (!checkPermission(req)) {
             return res.status(403).json({success: false, message: error.api.NO_PERMISSION})
         }
         const groupNumber = req.session.user.groupNumber;
         const wbName = req.body.wbName;
-        const onlyFilled = req.body.onlyFilled || false;
+        const onlyFilled = req.body.onlyFilled === undefined; // default true
         const queryData = req.body.queryData;
-        const tasks = [];
-        const result = {};
-        for (let wsName in queryData) {
-            const currSheet = queryData[wsName];
-            result[wsName] = [];
-            for (let i = 0; i < currSheet.length; i++) {
-                tasks.push(async () => {
-                    const data = await getDataAtLocation(onlyFilled, groupNumber, wbName, wsName, currSheet[i][0], currSheet[i][1]);
-                    result[wsName].push(data);
-                    console.log(data)
-                });
-            }
-        }
+        const includeAttCatId = req.body.includeAttCatId || false; // default false
 
-        async.parallel(tasks, (err) => {
+        const data = await getDataAtLocation(includeAttCatId, onlyFilled, groupNumber, wbName, queryData);
+        res.json({success: true, result: data});
+    },
+
+    get_workbook_names: (req, res, next) => {
+        if (!checkPermission(req)) {
+            return res.status(403).json({success: false, message: error.api.NO_PERMISSION})
+        }
+        Workbook.find({}, {name: 1}, (err, docs) => {
             if (err) {
-                console.log(err)
+                console.log(err);
+                return res.status(400).json({success: false, message: err});
             }
-            res.json({success: true, result: result})
+            docs = docs.reduce((arr, obj) => {arr.push(obj.name); return arr;}, []);
+            res.json({success: true, names: docs})
+        })
+    },
+
+    get_workbook_query_detail: (req, res, next) => {
+        if (!checkPermission(req)) {
+            return res.status(403).json({success: false, message: error.api.NO_PERMISSION})
+        }
+        Workbook.findOne({name: req.query.name}, {data: 1, attMap: 1, catMap:1}, (err, doc) => {
+            if (err) {
+                console.log(err);
+                return res.status(400).json({success: false, message: err});
+            }
+            const names = [], att = {}, cat = {};
+            if (!doc) {
+                return res.json({success: false, message: 'workbook name: ' + req.query.name + ' does not exist.'})
+            }
+            for (let sheetNo in doc.data) {
+                names.push(doc.data[sheetNo].name)
+            }
+            for (let sheetNo in doc.catMap) {
+                cat[sheetNo] = Object.keys(doc.catMap[sheetNo]);
+            }
+            for (let sheetNo in doc.attMap) {
+                att[sheetNo] = Object.keys(doc.attMap[sheetNo]);
+            }
+            res.json({success: true, worksheetNames: names, cat: cat, att: att})
         })
     },
 
--- routes/workbook-query.js
+++ routes/workbook-query.js
@@ -7,4 +7,12 @@ const error = require('../config/error');
 // GET Find a workbook in current group
 router.post('/api/query/workbook', workbookQueryController.workbook_query);
 
+router.get('/query/workbook', (req, res, next) => {
+    res.render('sidebar/workbookQuery.ejs', {user: req.session.user});
+});
+
+router.get('/api/query/workbook/names', workbookQueryController.get_workbook_names);
+
+router.get('/api/query/workbook/detail', workbookQueryController.get_workbook_query_detail);
+
 module.exports = router;
