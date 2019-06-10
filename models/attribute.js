41aab8dc6b58d5807b614a02d0c6401b3b73ffad
daviddongguo <david.dong.guo@gmail.com>
2019-06-10 14:32:22 -0400

    [change] add one property to attributeSchema: id, not autoincrement.

--- models/attribute.js
+++ models/attribute.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
-const AutoIncrement = require('mongoose-sequence')(mongoose);
+// const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 let attributeSchema = new mongoose.Schema({
+    id: {type: Number, required: true},
     attribute: {type: String},
     groupNumber: {type: Number, required: true}
 });
 
-attributeSchema.plugin(AutoIncrement, {inc_field: 'id', id: 'attributeId'});
+// attributeSchema.plugin(AutoIncrement, {inc_field: 'id', id: 'attributeId'});
 
 module.exports = mongoose.model('Attribute', attributeSchema);
