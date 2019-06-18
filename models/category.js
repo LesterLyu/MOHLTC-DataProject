77ea18cbd914827e1e561dc9c57dbae6f6bacc7a
daviddongguo <david.dong.guo@gmail.com>
2019-06-18 08:48:48 -0400

    [update]  adjust the category model

--- models/category.js
+++ models/category.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
-const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 let categorySchema = new mongoose.Schema({
-    category: {type: String},
-    groupNumber: {type: Number, required: true}
+    id: { type: Number, required: true, index: true, unique: true},
+    category: {type: String, required: true},
+    groupNumber: {type: Number, required: true},
+    description: {type: String},
 });
-
-// categorySchema.plugin(AutoIncrement, {inc_field: 'id', id: 'categoryId'});
 
 module.exports = mongoose.model('Category', categorySchema);
