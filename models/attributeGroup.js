3453888807fc171c47f39deea347e6fd929a4f30
daviddongguo <david.dong.guo@gmail.com>
2019-06-24 15:50:43 -0400

    [new] add tow models: attribute group and category group

---
+++ models/attributeGroup.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+let attributeGroupSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    groupNumber: {type: Number, required: true},
+    attributeIds: {type: Array}
+});
+
+module.exports = mongoose.model('AttributeGroup', attributeGroupSchema);
